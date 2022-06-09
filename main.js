c2c456c4f1da8d8a8119b4c84bc538544216574c
Maria-developer <94925262+Maria-developer@users.noreply.github.com>
2022-06-09 15:08:22 -0400

    Created Vue app

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: '80% cotton, 20% polyester, Gender-neutral'
+        }
+    }
+})
