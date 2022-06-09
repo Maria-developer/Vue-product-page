8a7b6aed59e0a67d404d7a63c0289f35e6127101
Maria-developer <94925262+Maria-developer@users.noreply.github.com>
2022-06-09 15:52:48 -0400

    Display product image

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: '80% cotton, 20% polyester, Gender-neutral'
+            description: '80% cotton, 20% polyester, Gender-neutral',
+            image: './assets/images/socks_green.jpg',
         }
     }
 })
